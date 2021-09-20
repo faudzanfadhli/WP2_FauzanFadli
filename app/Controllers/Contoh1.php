1feb9ba656fc558381f13df4653b00ee050def62
faudzanfadhli <faudzan.fadhli.of.pd.m.fc@gmail.com>
2021-09-20 18:06:39 +0700

    First commit.

---
+++ app/Controllers/Contoh1.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Contoh1 extends BaseController
+{
+	public function index()
+	{
+		echo "<h1>Biodata</h1>";
+		echo "<p>NIM: 12201340<br>Nama: Fauzan Fadli<br>Kelas: 12.3B.03<br>Kampus: UBSI Cimone<br>Alamat: Bekasi<br>E-mail: 12201340@bsi.ac.id</p>";
+
+	}
+}
